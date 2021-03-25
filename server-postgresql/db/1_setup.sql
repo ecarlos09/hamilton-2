64449a9edbc33edf6fecbc60091d6bcb602ce2ac
Elwin <elwin.carlos09@gmail.com>
2021-03-25 11:44:28 +0000

    stub file structure and add initial data

---
+++ server-postgresql/db/1_setup.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    git_username varchar(100) NOT NULL,
+    repos int NOT NULL,
+    partner_id int
+);
+
+DROP TABLE IF EXISTS partners;
+
+CREATE TABLE partners (
+     id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    location varchar(255)
+);
---
+++ server-postgresql/db/2_seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO partners (name, location)
+VALUES
+    ('Coca Cola', 'Marylebone'),
+    ('Babylon Health', 'Chelsea');
+
+INSERT INTO students (name, username, repos, partner_id)
+VALUES
+    ('Aaron', 'HeyAero', 26, null),
+    ('Adil', 'AdilIqbal95', 12, null),
+    ('Chris', 'cwesterduin', 22, null),
+    ('Emmanuel', 'emmanuel-sobamowo', 6, null),
+    ('Elwin', 'ecarlos09', 38, null),
+    ('Faisal', 'FaisalY12', 16, null),
+    ('Greta', 'gretaivan', 22, null),    
+    ('James', 'JamesWheadon', 19, null),
+    ('Natalie', 'natbibi', 13, null),
+    ('Pearl', 'pearlhamilton', 26, null),
+    ('Ravil', 'ravil-96', 12, null),
+    ('Rishi', 'theduckfliesagain', 17, null),
+    ('Ryan', 'RKuppan97', 18, null),
+    ('Roselyn', 'roselynle', 11, null),
+    ('Semhar', 'sem-the-dev', 22, null);
