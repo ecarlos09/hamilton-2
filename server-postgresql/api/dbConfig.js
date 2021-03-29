1013b94fa91082aaa6e33aaf61af47d9813d89ca
Elwin <elwin.carlos09@gmail.com>
2021-03-29 11:02:15 +0100

    create run function that returns SQL queries

--- server-postgresql/api/dbConfig.js
+++ server-postgresql/api/dbConfig.js
@@ -2,4 +2,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool();
 
-module.exports = pool;
+function run(q, values, callback) {
+    return pool.query(q, values,callback);
+};
+
+module.exports = { run };
