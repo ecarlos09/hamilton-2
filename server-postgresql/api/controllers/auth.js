55d32903ad82026079dc4e28b3f5f0b4e508212e
Elwin <elwin.carlos09@gmail.com>
2021-03-29 10:55:53 +0100

    add login route

--- server-postgresql/api/controllers/auth.js
+++ server-postgresql/api/controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
+//Create registration route
 router.post('/register', async (req,res) => {
     try {
         //Generate salt
@@ -14,5 +15,23 @@ router.post('/register', async (req,res) => {
         res.status(201).json({msg: 'New user created!'});
     } catch(err) {
         res.status(500).json({err});
+    }
+});
+
+//Create login route
+router.post('/login', async (req,res) => {
+    try {
+        const user  = await User.findByEmail(req.body.email);
+        if(!user) {
+            throw new Error('There is no user with this email! Double check your spelling, or try another address.')
+        };
+        const authed = bcrypt.compare(req.body.password, user.passwordDigest);
+        if(authed) {
+            res.status(200).json({user: user.username});            
+        } else {
+            throw new Error ('User could not be authenticated.  Retry login.');
+        }
+    } catch(err) {
+        res.status(401).json({err});
     }
 });
