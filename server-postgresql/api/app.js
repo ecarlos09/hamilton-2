ab5fa511635384794484dd796f2a58d1c2f88a95
Elwin <elwin.carlos09@gmail.com>
2021-03-29 15:22:31 +0100

    add middleware

--- server-postgresql/api/app.js
+++ server-postgresql/api/app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const cors = require('cors');
 
 const api = express();
+
 api.use(cors());
 api.use(express.json());
 
 const studentRoutes = require('./controllers/students');
 const partnerRoutes = require('./controllers/partners');
 const authRoutes = require('./controllers/auth');
+const userRoutes = require('./controllers/users');
 
 api.use('/students', studentRoutes);
 api.use('/partners', partnerRoutes);
 api.use('/auth', authRoutes);
+api.use('/users', userRoutes);
 
 api.get('/', (req,res) => res.send('Welcome to the Hamilton Cohort!'));
 
